16913103ea9cbf0fe317dfdff1a718da11de15fa
Josefine <josefineeriksson@live.se>
2026-02-11 12:08:01 +0100

    google link

--- src/components/trustmary.tsx
+++ src/components/trustmary.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
 
 export default function TrustmaryWidget() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -27,11 +29,25 @@ export default function TrustmaryWidget() {
   }, [])
 
   return (
-    <section className="py-16 bg-white dark:bg-gray-800">
+    <section className="py-16 ">
       <div className="container mx-auto px-4">
         <div ref={containerRef}>
           {!isLoaded && <p className="text-center">Laddar recensioner...</p>}
         </div>
+      </div>
+
+      <div className="flex justify-center">
+       <Button asChild>
+          <a
+            href="https://www.google.com/search?sa=X&sca_esv=8ed7b9c9fed93e72&sxsrf=ANbL-n6PfwE85w1HXk0SQOFdtdEfW0ENuA:1770806998886&q=Skeninge+VVS+AB+Recensioner&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDY0NjY3MzYxMTcxtTQ1MTQ3NtzAyPiKUTo4OzUvMy89VSEsLFjB0UkhKDU5Na84Mz8vtWgRKz5ZAPQsD1tXAAAA&rldimm=13133763447459541731&tbm=lcl&hl=sv-SE&ved=2ahUKEwiKr4mRotGSAxXYHRAIHUxzIpMQ9fQKegQITBAG&biw=2560&bih=1271&dpr=1#lkt=LocalPoiReviews"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center font-bold bg-red-800 hover:scale-105 hover:bg-red-900 transition-transform duration-200 px-6 py-3 rounded text-white"
+          >
+            Lämna en recension!
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </a>
+        </Button>
       </div>
     </section>
   )
